d33bb0853fffb5a63f90df58fcc40f08a361b0e9
3rd-Eden <info@3rd-Eden.com>
2013-06-25 19:41:50 +0200

    [minor] moving the client to incoming/outgoing pattern.

--- primus.js
+++ primus.js
@@ -28,24 +28,24 @@
   Primus.prototype.initialise = function initalise() {
     var primus = this;
 
-    this.on('primus::data', function message(data) {
+    this.on('incoming::data', function message(data) {
       primus.decoder(data, function decoding(err, packet) {
         //
         // Do a "save" emit('error') when we fail to parse a message. We don't
         // want to throw here as listening to errors should be optional.
         //
-        if (err) return primus.listeners('error').length && socket.emit('error', err);
+        if (err) return primus.listeners('error').length && primus.emit('error', err);
         primus.emit('data', packet);
       });
     });
 
-    this.on('primus::end', function end() {
+    this.on('incoming::end', function end() {
       this.reconnect(function (fail, backoff) {
         primus.backoff = backoff; // Save the opts again of this backoff.
-        if (fail) return self.emit('end', fail);
+        if (fail) return primus.emit('end', fail);
 
         // Try to re-open the connection again.
-        primus.emit('primus::reconnect');
+        primus.emit('outgoing::reconnect');
       }, primus.backoff);
     });
 
@@ -58,7 +58,9 @@
    * @api private
    */
   Primus.prototype.connect = function connect() {
-    this.emit('primus::connect', this.uri());
+    this.emit('outgoing::connect', this.uri());
+
+    return this;
   };
 
   /**
@@ -67,7 +69,10 @@
    * @api public
    */
   Primus.prototype.end = function end() {
+    this.emit('outgoing::end');
+    this.emit('end');
 
+    return this;
   };
 
   /**
@@ -81,11 +86,11 @@
   Primus.prototype.backoff = function backoff(callback, opts) {
     opts = opts || {};
 
-    opts.maxDelay = opts.maxDelay || Infinity;  // Maximum delay
-    opts.minDelay = opts.minDelay || 500;       // Minimum delay
-    opts.retries = opts.retries || 25;          // Amount of allowed retries
-    opts.attempt = (+opts.attempt || 0) + 1;    // Current attempt
-    opts.factor = opts.factor || 2;             // Backoff factor
+    opts.maxDelay = opts.maxDelay || Infinity;  // Maximum delay.
+    opts.minDelay = opts.minDelay || 500;       // Minimum delay.
+    opts.retries = opts.retries || 25;          // Amount of allowed retries.
+    opts.attempt = (+opts.attempt || 0) + 1;    // Current attempt.
+    opts.factor = opts.factor || 2;             // Backoff factor.
 
     // Bailout if we are about to make to much attempts. Please note that we use
     // `>` because we already incremented the value above.
@@ -142,7 +147,7 @@
     server.push(this.url.host, this.pathname.slice(1));
 
     //
-    // Optionally add a search query
+    // Optionally add a search query.
     //
     if (this.url.search) server.push(this.url.search);
     return server.join('/');
@@ -215,7 +220,7 @@
       // as we cannot be certain that all frameworks fix these issues.
       //
       setTimeout(function timeout() {
-        primus.emit('primus::'+ event, data);
+        primus.emit('incoming::'+ event, data);
       }, 0);
     };
   };
@@ -224,8 +229,8 @@
   // These libraries are automatically are automatically inserted at the
   // serverside using the Primus#library method.
   //
-  Primus.prototype.client = null; // @import {primus::transport};
   Primus.prototype.pathname = null; // @import {primus::pathname};
+  Primus.prototype.client = null; // @import {primus::transport};
   Primus.prototype.encoder = null; // @import {primus::encoder};
   Primus.prototype.decoder = null; // @import {primus::decoder};
   Primus.prototype.version = null; // @import {primus::version};
