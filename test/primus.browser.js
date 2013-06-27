fd90246dce0899f4a2b10ebee9bfb3d3446afaaf
3rd-Eden <info@3rd-Eden.com>
2013-06-27 13:13:27 +0200

    [test] Adding more testlings

--- test/primus.browser.js
+++ test/primus.browser.js
@@ -5,7 +5,44 @@ describe('primus.js', function () {
     , assert = require('assert')
     , primus = new Primus('/');
 
+  //
+  // Simple type helper.
+  //
+  assert.is = assert.is || function (value, type) {
+    var internal = Object.prototype.toString.call(type).toLowerCase();
+    assert.ok(internal === '[object '+ value.toLowerCase() +']');
+  };
+
   it('is exposed as function', function () {
-    assert.ok(typeof Primus === 'function');
+    assert.is('function', Primus);
+  });
+
+  describe('#on', function () {
+    it('adds an "foo" event', function () {
+      assert.ok(!('foo' in primus.events));
+
+      function foo() { }
+      primus.on('foo', foo);
+
+      assert.ok('foo' in primus.events);
+      assert.is('array', primus.events.foo);
+      assert.ok(primus.events.foo.length);
+      assert.ok(primus.events.foo[0] === foo);
+    });
+  });
+
+  describe('#listeners', function () {
+    it('always returns an Array', function () {
+      assert.is('array', primus.listeners('bar'));
+    });
+
+    it('returns the added event listener', function () {
+      function fooo() {}
+      primus.on('fooo', fooo);
+
+      assert.is('array', primus.listeners('fooo'));
+      assert.ok(primus.listeners('fooo').length === 1);
+      assert.ok(primus.listeners('fooo')[0] === fooo);
+    });
   });
 });
