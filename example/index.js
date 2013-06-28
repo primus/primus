b232a64775ccb1f76e7ce455a702ed7c17d486ee
3rd-Eden <info@3rd-Eden.com>
2013-06-28 15:54:01 +0200

    [doc] Adding an simple primus server example.

---
+++ example/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+//
+// Require all dependencies.
+//
+// Argh is an light weight argument parser that we use in this example to change
+// between parsers and transformers. The following CLI arguments are accepted:
+//
+// --transformer <value>  (the name of the transformer we want to use)
+// --parser <value>       (the name of the parser we want to use)
+// --port <value>         (the port number we want to listen to)
+//
+var argh = require('argh').argv
+  , Primus = require('../')
+  , server
+  , primus;
+
+//
+// Some build in Node.js modules that we need:
+//
+var http = require('http')
+  , fs = require('fs');
+
+//
+// Create a basic server that will send the compiled library or a basic HTML
+// file which we can use for testing.
+//
+server = http.createServer(function server(req, res) {
+  if (req.url === '/library') {
+    res.setHeader('Content-Type', 'text/javascript');
+    return res.end(primus.library());
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
+});
+
+//
+// Now that we've setup our basic server, we can setup our Primus server.
+//
+primus = new Primus(server, { transformer: argh.transformer, parser: argh.parser });
+
+//
+// Listen for new connections and send data
+//
+primus.on('connection', function connection(spark) {
+  console.log('new connection');
+
+  spark.on('data', function data(packet) {
+    console.log('incoming:', packet);
+
+    //
+    // Close the connection.
+    //
+    if (packet === 'end') spark.end();
+
+    //
+    // Echo the responses.
+    //
+    if (packet.echo) spark.write(packet.echo);
+
+    //
+    // Pipe in some data.
+    //
+    if (packet.pipe) fs.createReadStream(__dirname + '/index.html').pipe(spark, {
+      autoClose: false
+    });
+  });
+});
+
+//
+// Everything is ready, listen to a port number to start the server.
+//
+server.listen(+argh.port || 8080);
