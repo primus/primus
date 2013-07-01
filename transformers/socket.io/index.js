55c64103ecd151fa717d71ec8376f7ae0348c243
3rd-Eden <info@3rd-Eden.com>
2013-07-01 21:21:43 +0200

    [test][fix] Stupid NPM edgecase that optimzes dependencies.

--- transformers/socket.io/index.js
+++ transformers/socket.io/index.js
@@ -5,6 +5,17 @@ var path = require('path')
   , library = path.join(directory, 'node_modules/socket.io-client/dist/socket.io.js');
 
 //
+// Testing edge-case. We're also installing the socket.io-client and npm tries
+// to be a smarty pants and optimize our packages and removes the
+// socket.io-client from the dependencies directory as it's already in the
+// parents `node_module`.
+//
+try {
+  directory = path.dirname(require.resolve('socket.io-client'));
+  library = path.join(directory, '../dist/socket.io.js');
+} catch (e) {}
+
+//
 // Expose the module as new Transformer instance.
 //
 module.exports = require('../../transformer').extend({
