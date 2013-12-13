3368df6bd489391721ebcf6c2c2fb101fe6fd4c3
3rd-Eden <info@3rd-Eden.com>
2013-12-13 11:00:41 +0100

    [test] Adding reproducible test case for #110

---
+++ issues/110/index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var Primus = require('../../')
+  , http = require('http')
+  , spdy = require('spdy')
+  , argh = require('argh')
+  , path = require('path')
+  , fs = require('fs');
+
+//
+// Create a server where we will connect from.
+//
+http.createServer(function handle(req, res) {
+  if (~req.url.indexOf('primus')) throw new Error('No Primus requests should be served');
+
+  res.setHeader('Content-Type', 'text/html');
+  fs.createReadStream(__dirname +'/index.html').pipe(res);
+}).listen(5000);
+
+//
+// Create the HTTPS server where we can attach our primus server on.
+//
+var server = spdy.createServer({
+  key: fs.readFileSync(path.join(__dirname, '../../ssl/server.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../../ssl/server.crt')),
+  windowSize: 1024 * 1024,
+  autoSpdy31: false
+}, function handle(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  fs.createReadStream(__dirname +'/index.html').pipe(res);
+});
+
+//
+// Attach the primus server on spdy server.
+//
+var primus = new Primus(server, {
+  transformer: argh.argv.transformer || 'engine.io',
+  parser: argh.argv.parser || 'JSON'
+});
+
+primus.on('connection', function connection(spark) {
+  console.log(spark.id, ' has connected');
+
+  spark.on('data', function data(msg) {
+    console.log(spark.id, ' send: ', msg);
+  });
+
+  spark.on('end', function end() {
+    console.log(spark.id, ' has disconnected');
+  });
+});
+
+server.listen(443, function connected() {
+  console.log('original server listening on https://localhost:443');
+  console.log('proxy server listening on http://localhost:5000');
+});
---
+++ issues/README.md
@@ -0,0 +1,7 @@
+# Github issues
+
+This is our reproduction play ground. Some of these bugs are really hard to
+reproduce using a test suite as they require a lot of different variables to
+expose a certain edge case. Most of these could require interaction of
+a browser/human in order to trigger these. So there isn't a way of automating
+these tests.
