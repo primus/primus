81369a5b13af5918e5eb9629c36254b000fd5474
Luigi Pinca <luigipinca@gmail.com>
2014-03-04 19:45:08 +0100

    [refactor] Refactor the procedure used to build the Engine.IO client
    Do not use the `engine.io-client` dev dependency to build the client. Clone the repo, checkout to the latest tag and build from there instead.
    This gives more flexibility and ensures that the build is done against the latest available version.

--- transformers/engine.io/update.sh
+++ transformers/engine.io/update.sh
@@ -1,28 +1,20 @@
-#!/usr/bin/env node
+#!/usr/bin/env bash
 
-'use strict';
+set -e
 
-var fs = require('fs')
-  , globalWrap = require('global-wrap')
-  , main = require.resolve('engine.io-client')
-  , path = require('path')
-  , tmpdir = path.dirname(main);
+CURRENTDIR=$(pwd)
+DESTDIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+TEMPDIR=$(mktemp -d 2> /dev/null || mktemp -d -t 'tmp')
 
-//
-// Build Engine.IO client using the `global-wrap` module.
-// This generates a bundle and exposes it as a property of the global object.
-// The difference with the official build is that this bundle does not use a
-// UMD pattern. The Primus client, in fact, expects to have a global `eio`
-// available and the UMD pattern prevents this global from being set when
-// RequireJS is used. See issue #157.
-//
-globalWrap({
-  constructorOptions: { builtins: false },
-  global: 'eio',
-  main: main,
-  tmpDir: tmpdir,
-}, function (err, output) {
-  if (err) throw err;
-  var dest = path.resolve(__dirname, 'library.js');
-  fs.writeFileSync(dest, output);
-});
+cleanup () {
+  cd $CURRENTDIR
+  [ -d $TEMPDIR ] && rm -rf $TEMPDIR
+}
+
+trap cleanup INT TERM EXIT
+
+git clone https://github.com/LearnBoost/engine.io-client.git $TEMPDIR
+cd $TEMPDIR
+git checkout $(git describe --tags --abbrev=0)
+NODE_ENV=production npm install
+$DESTDIR/globalify.sh $TEMPDIR
