aaf108c60fd1fe98b3b1fe9c20b2166cbc565b80
3rd-Eden <info@3rd-Eden.com>
2014-03-11 00:05:33 +0100

    [major] HTTP Access Control (CORS)

--- transformer.js
+++ transformer.js
@@ -2,6 +2,7 @@
 
 var querystring = require('querystring').parse
   , EventEmitter = require('eventemitter3')
+  , access = require('access-control')
   , url = require('url').parse;
 
 //
@@ -23,6 +24,11 @@ function Transformer(primus) {
   this.specfile = null;         // Path to the Primus specification.
   this.service = null;          // Stores the real-time service.
   this.buffer = null;           // Buffer of the library.
+
+  //
+  // Configure the HTTP Access Control (CORS) handling.
+  //
+  this.cors = access(primus.options.cors);
 
   EventEmitter.call(this);
   this.initialise();
@@ -139,6 +145,8 @@ Transformer.prototype.initialise = function initialise() {
  * @api private
  */
 Transformer.prototype.request = function request(req, res) {
+  if (this.cors(req, res)) return; // Cors request handled.
+
   if (!this.test(req)) return this.emit('previous::request', req, res);
   if (req.uri.pathname === this.primusjs) return this.emit('static', req, res);
   if (req.uri.pathname === this.specfile) return this.emit('spec', req, res);
