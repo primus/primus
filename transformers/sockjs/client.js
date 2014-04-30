32f7e70883172a58e893512f79b2e964b2f6c18c
Alex Mingoia <talk@alexmingoia.com>
2014-04-30 16:51:34 -0700

    Emit error event for close event codes above 1000.

--- transformers/sockjs/client.js
+++ transformers/sockjs/client.js
@@ -48,7 +48,7 @@ module.exports = function client() {
     socket.onopen = primus.emits('open');
     socket.onerror = primus.emits('error');
     socket.onclose = function (e) {
-      var event = e && e.code === 1002 ? 'error' : 'end';
+      var event = e && e.code > 1000 ? 'error' : 'end';
 
       //
       // The timeout replicates the behaviour of primus.emits so we're not
