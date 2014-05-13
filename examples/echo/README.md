cecdc3623bbd29f3a68d9fd6cc4f4494555dc970
3rd-Eden <info@3rd-Eden.com>
2014-05-13 22:46:33 +0200

    [example] Added minimal echo example

---
+++ examples/echo/README.md
@@ -0,0 +1,24 @@
+# Echo
+
+The most minimal example of doing an echo using Primus.
+
+## Installation
+
+Simply run:
+
+```
+npm install .
+```
+
+In the current directory and start the server using:
+
+```
+node index.js
+```
+
+And open `http://localhost:8080` in your browser to send messages to the server
+which it will echo back.
+
+## License
+
+MIT
---
+++ examples/echo/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//
+// Create the HTTP server and serve our index.html
+//
+var server = require('http').createServer(function incoming(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  require('fs').createReadStream(__dirname + '/index.html').pipe(res);
+});
+
+//
+// Attach Primus to the HTTP server.
+//
+var Primus = require('primus')
+  , primus = new Primus(server);
+
+//
+// Listen for connections and echo the events send.
+//
+primus.on('connection', function connection(spark) {
+  spark.on('data', function received(data) {
+    console.log(spark.id, 'received message:', data);
+    spark.write(data);
+  });
+});
+
+server.listen(8080, function () {
+  console.log('Open http://localhost:8080 in your browser');
+});
