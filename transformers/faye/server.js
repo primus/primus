b8f4401aa1fe2513427ed4e3311ec45729bb6fb3
Luigi Pinca <luigipinca@gmail.com>
2014-06-26 21:46:44 +0200

    [fix] Create the spark only when the `open` event is fired

--- transformers/faye/server.js
+++ transformers/faye/server.js
@@ -15,37 +15,48 @@ module.exports = function server() {
     , Spark = this.Spark;
 
   //
-  // Listen to upgrade requests
+  // Listen to upgrade requests.
   //
   this.on('upgrade', function upgrade(req, socket, head) {
     if (!Faye.isWebSocket(req)) return socket.destroy();
 
-    var websocket = new Faye(req, socket, head)
-      , spark = new Spark(
-          req.headers                             // HTTP request headers.
-        , req                                     // IP address location.
-        , parse(req.url).query                    // Optional query string.
-        , null                                    // We don't have an unique id.
-        , req                                     // Reference to the HTTP req.
-    );
+    var websocket = new Faye(req, socket, head);
 
-    spark.on('outgoing::end', function end() {
-      if (websocket) websocket.close();
-    }).on('outgoing::data', function write(data) {
-      if ('string' === typeof data) return websocket.send(data);
+    //
+    // The WebSocket handshake is complete only when the `open` event is fired.
+    //
+    websocket.on('open', function open() {
+      var spark = new Spark(
+          req.headers          // HTTP request headers.
+        , req                  // IP address location.
+        , parse(req.url).query // Optional query string.
+        , null                 // We don't have an unique id.
+        , req                  // Reference to the HTTP req.
+      );
 
-      websocket.send(data, { binary: true });
+      spark.on('outgoing::end', function end() {
+        if (websocket) websocket.close();
+      }).on('outgoing::data', function write(data) {
+        if ('string' === typeof data) return websocket.send(data);
+
+        websocket.send(data, { binary: true });
+      });
+
+      websocket.on('error', spark.emits('error'));
+      websocket.on('message', spark.emits('data', function parse(evt) {
+        return evt.data;
+      }));
+      websocket.on('close', spark.emits('end', function close() {
+        websocket.removeAllListeners();
+        websocket = null;
+      }));
     });
+  });
 
-    websocket.on('error', spark.emits('error'));
-    websocket.on('message', spark.emits('data', function parse(evt) {
-      return evt.data;
-    }));
-    websocket.on('close', spark.emits('end', function close() {
-      websocket.removeAllListeners();
-      websocket = null;
-    }));
-  }).on('request', function request(req, res) {
+  //
+  // Listen to non-upgrade requests.
+  //
+  this.on('request', function request(req, res) {
     res.writeHead(426, { 'content-type': 'text/plain' });
     res.end(http.STATUS_CODES[426]);
   });
