fcd323c9005392359028a50db8bf5ec790e43623
Luigi Pinca <luigipinca@gmail.com>
2014-09-20 10:51:29 +0200

    [patch] Patch the Socket.IO client to make it work when it is wrapped in a function

--- transformers/socket.io/library.js
+++ transformers/socket.io/library.js
@@ -1,6 +1,5 @@
 /*! Socket.IO.js build:0.9.17, development. Copyright(c) 2011 LearnBoost <dev@learnboost.com> MIT Licensed */
 
-var io = ('undefined' === typeof module ? {} : module.exports);
 (function() {
 
 /**
