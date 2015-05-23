063b064f15e448e391a00b31ee2191b77af81370
Luigi Pinca <luigipinca@gmail.com>
2015-05-23 12:13:55 +0200

    [test] Test support of literal IPv6 addresses in the connection URL

--- test/transformer.base.js
+++ test/transformer.base.js
@@ -67,6 +67,16 @@ module.exports = function base(transformer, pathname, transformer_name) {
         });
       });
 
+      if (
+          'unixdomainwebsockets' !== transformer_name
+        && 'browserchannel' !== transformer
+        && !/^v0\.(?:8|10)/.test(process.version)
+      ) it('supports literal IPv6 addresses in the connection URL', function (done) {
+        var socket = new Socket('http://[::1]:'+ server.portnumber);
+
+        socket.on('open', socket.end).on('end', done);
+      });
+
       it('exposes a .socket property', function (done) {
         var socket = new Socket(server.addr);
 
