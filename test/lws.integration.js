0adea28e8b179970c141c1e0b49aca7f139cf3e3
Luigi Pinca <luigipinca@gmail.com>
2016-04-28 08:12:03 +0200

    [test] Run lws integration tests only on node 4 and 5

--- test/lws.integration.js
+++ test/lws.integration.js
@@ -1,6 +1,8 @@
 'use strict';
 
-// lws do only support ABI version 46+
-if (process.versions.modules >= 46) {
+//
+// lws only supports ABI version 46 and 47.
+//
+if (process.versions.modules === 46 || process.versions.modules === 47) {
   require('./transformer.base')('lws');
 }
